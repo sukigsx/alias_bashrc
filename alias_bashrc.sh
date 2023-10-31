c51a79f0445f04e28dc9e8f6ea5eca3587813c1a
sukigsx <sukigsx.mbsistemas@gmail.com>
2023-10-31 18:05:09 +0100

    Update alias_bashrc.sh

--- alias_bashrc.sh
+++ alias_bashrc.sh
@@ -144,7 +144,7 @@ clear
 echo ""
 echo -e "${rosa}"; figlet -c Instalacion; echo -e "${borra_colores}"
 echo -e ""
-echo 'source /home/$(whoami)/.config/alias_bashrc_sukigsx' >> /home/$(whoami)/.bashrc
+echo 'source /home/$(whoami)/.config/alias_bashrc/alias_bashrc_sukigsx' >> /home/$(whoami)/.bashrc
 mkdir /home/$(whoami)/.config/alias_bashrc
 wget -O /home/$(whoami)/.config/alias_bashrc/alias_bashrc.config https://github.com/sukigsx/alias_bashrc/blob/main/alias_bashrc.config
 
