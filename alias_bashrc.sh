52012086b12c34e5f42cbba536e520d4463cd1ed
sukigsx <sukigsx.mbsistemas@gmail.com>
2023-10-31 17:47:09 +0100

    Update alias_bashrc.sh

--- alias_bashrc.sh
+++ alias_bashrc.sh
@@ -129,18 +129,16 @@ else
 fi
 
 sleep 2
-# Ruta al archivo .bashrc del usuario actual
-bashrc_file='/home/$(whoami)/.bashrc'
-
-# Línea que estamos buscando
-line_to_search="source /home/$(whoami)/.config/alias_bashrc_sukigsx"
 
 # Verificar si la línea existe en el archivo .bashrc
 if grep -qF 'source /home/$(whoami)/.config/alias_bashrc_sukigsx' "/home/$(whoami)/.bashrc"; then
-    echo " Si esta instalado."
-else
-    echo " No esta instalado."
+    echo -e " El script $0 ya esta instalado en tu sistema."
+    echo -e " Puedes utilizar los comandos para utilizarlo."
+    echo -e " Comando = (opciones_alias) para listar las opciones."
+    read p
 fi
+
+echo " No esta instalado."
 
 
 
