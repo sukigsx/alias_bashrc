775fe114abf4dddb2ef4c227d06f405fd2365d0b
sukigsx <sukigsx.mbsistemas@gmail.com>
2025-10-25 11:40:16 +0200

    a

--- alias_bashrc.sh
+++ alias_bashrc.sh
@@ -1,9 +1,46 @@
 #!/bin/bash
 
-#colores
-#ejemplo: echo -e "${verde} La opcion (-e) es para que pille el color.${borra_colores}"
+#VARIABLES PRINCIPALES
+# con export son las variables necesarias para exportar al los siguientes script
+#variables para el menu_info
 
-rojo="\e[0;31m\033[1m" #rojoo
+export NombreScript="alias_bashrc"
+export DescripcionDelScript="herramienta para Crear/Modificar/Backups de tu alias"
+export Correo=""
+export Web=""
+export version="1.0"
+conexion="Sin comprobar"
+software="Sin comprobar"
+actualizado="No se ha podido comprobar la actualizacion del script"
+
+# VARIABLE QUE RECOJEN LAS RUTAS
+ruta_ejecucion=$(dirname "$(readlink -f "$0")") #es la ruta de ejecucion del script sin la / al final
+ruta_escritorio=$(xdg-user-dir DESKTOP) #es la ruta de tu escritorio sin la / al final
+
+# VARIABLES PARA LA ACTUALIZAION CON GITHUB
+NombreScriptActualizar="alias_bashrc.sh" #contiene el nombre del script para poder actualizar desde github
+DireccionGithub="https://github.com/sukigsx/alias_bashrc.git" #contiene la direccion de github para actualizar el script
+
+#VARIABLES DE SOFTWARE NECESARIO
+#which git diff ping figlet xdotool nano fzf
+# Asociamos comandos con el paquete que los contiene [comando a comprobar]="paquete a instalar"
+    declare -A requeridos
+    requeridos=(
+        [which]="which"
+        [git]="git"
+        [diff]="diff"
+        [ping]="ping"
+        [figlet]="figlet"
+        [nano]="nano"
+        [fzf]="fzf"
+        [xdotool]="xdotool"
+    )
+    )
+    )
+
+
+#colores
+rojo="\e[0;31m\033[1m" #rojo
 verde="\e[;32m\033[1m"
 azul="\e[0;34m\033[1m"
 amarillo="\e[0;33m\033[1m"
@@ -16,19 +53,119 @@ trap ctrl_c INT
 function ctrl_c()
 {
 clear
-figlet -c Gracias por
-figlet -c utilizar mi
-figlet -c script
-#wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz >/dev/null 2>&1
+echo ""
+echo -e "${azul} GRACIAS POR UTILIZAR MI SCRIPT${borra_colores}"
+echo ""
+sleep 1
 exit
 }
 
+menu_info(){
+# muestra el menu de sukigsx
+echo ""
+echo -e "${rosa}            _    _                  ${azul}   Nombre del script${borra_colores} ($NombreScript)"
+echo -e "${rosa}  ___ _   _| | _(_) __ _ _____  __  ${azul}   Descripcion${borra_colores} ($DescripcionDelScript)"
+echo -e "${rosa} / __| | | | |/ / |/ _\ / __\ \/ /  ${azul}   Version            =${borra_colores} $version"
+echo -e "${rosa} \__ \ |_| |   <| | (_| \__ \>  <   ${azul}   Conexion Internet  =${borra_colores} $conexion"
+echo -e "${rosa} |___/\__,_|_|\_\_|\__, |___/_/\_\  ${azul}   Software necesario =${borra_colores} $software"
+echo -e "${rosa}                  |___/             ${azul}   Actualizado        =${borra_colores} $actualizado"
+echo -e ""
+echo -e "${azul} Contacto:${borra_colores} (Correo $Correo) (Web $Web)${borra_colores}"
+echo ""
+}
+
+
+actualizar_script(){
+    # actualizar el script
+    #para que esta funcion funcione necesita:
+    #   conexion a internet
+    #   la paleta de colores
+    #   software: git diff
+
+    git clone $DireccionGithub /tmp/comprobar >/dev/null 2>&1
+
+    diff $ruta_ejecucion/$NombreScriptActualizar /tmp/comprobar/$NombreScriptActualizar >/dev/null 2>&1
+
+
+    if [ $? = 0 ]
+    then
+        #esta actualizado, solo lo comprueba
+        echo ""
+        echo -e "${verde} El script${borra_colores} $0 ${verde}esta actualizado.${borra_colores}"
+        echo ""
+        chmod -R +w /tmp/comprobar
+        rm -R /tmp/comprobar
+        actualizado="SI"
+        sleep 2
+    else
+        #hay que actualizar, comprueba y actualiza
+        echo ""
+        echo -e "${amarillo} EL script${borra_colores} $0 ${amarillo}NO esta actualizado.${borra_colores}"
+        echo -e "${verde} Se procede a su actualizacion automatica.${borra_colores}"
+        sleep 3
+        cp -r /tmp/comprobar/* $ruta_ejecucion
+        chmod -R +w /tmp/comprobar
+        rm -R /tmp/comprobar
+        echo ""
+        echo -e "${amarillo} El script se ha actualizado, es necesario cargarlo de nuevo.${borra_colores}"
+        echo ""
+        sleep 2
+        exit
+    fi
+}
+
+
+software_necesario(){
+#funcion software necesario
+#para que funcione necesita:
+#   conexion a internet
+#   la paleta de colores
+#   software: which
+
+echo ""
+echo -e "${azul} Comprobando el software necesario.${borra_colores}"
+echo ""
+#which git diff ping figlet xdotool wmctrl nano fzf
+#########software="which git diff ping figlet nano gdebi curl konsole" #ponemos el foftware a instalar separado por espacion dentro de las comillas ( soft1 soft2 soft3 etc )
+for comando in "${!requeridos[@]}"; do
+        which $comando &>/dev/null
+        sino=$?
+        contador=1
+        while [ $sino -ne 0 ]; do
+            if [ $contador -ge 4 ] || [ "$conexion" = "no" ]; then
+                clear
+                echo ""
+                echo -e " ${amarillo}NO se ha podido instalar ${rojo}${requeridos[$comando]}${amarillo}.${borra_colores}"
+                echo -e " ${amarillo}Inténtelo usted con: (${borra_colores}sudo apt install ${requeridos[$comando]}${amarillo})${borra_colores}"
+                echo -e ""
+                echo -e " ${rojo}No se puede ejecutar el script sin el software necesario.${borra_colores}"
+                echo ""; read p
+                echo ""
+                exit 1
+            else
+                echo " Instalando ${requeridos[$comando]}. Intento $contador/3."
+                sudo apt install ${requeridos[$comando]} -y &>/dev/null
+                let "contador=contador+1"
+                which $comando &>/dev/null
+                sino=$?
+            fi
+        done
+        echo -e " [${verde}ok${borra_colores}] $comando (${requeridos[$comando]})."
+    done
+
+    echo ""
+    echo -e "${azul} Todo el software ${verde}OK${borra_colores}"
+    sleep 2
+}
+
+
+conexion(){
 #funcion de comprobar conexion a internet
 #para que funciones necesita:
 #   conexion ainternet
 #   la paleta de colores
 #   software: ping
-conexion(){
+
 if ping -c1 google.com &>/dev/null
 then
     conexion="SI"
@@ -41,98 +178,63 @@ else
 fi
 }
 
-#actualizar el script
-actualizar_script(){
-archivo_local="alias_bashrc.sh" # Nombre del archivo local
-ruta_repositorio="https://github.com/sukigsx/alias_bashrc.git" #ruta del repositorio para actualizar y clonar con git clone
+#logica de arranque
+#variables de resultado $conexion $software $actualizado
+#funciones actualizar_script, conexion, software_necesario
 
-# Obtener la ruta del script
-descarga=$(dirname "$(readlink -f "$0")")
-git clone $ruta_repositorio /tmp/comprobar >/dev/null 2>&1
+#logica para ejecutar o no ejecutar
+#comprobado conexcion
+#    si=actualizar_script
+#        si=software_necesario
+#            si=ejecuta, poner variables a sii todo
+#            no=Ya sale el solo desde la funcion
+#        no=software_necesario
+#            si=ejecuta, variables software="SI", conexion="SI", actualizado="No se ha podiso comprobar actualizacion de script"
+#            no=Ya sale solo desde la funcion
+#
+#    no=software_necesario
+#        si=ejecuta, variables software="SI", conexion="NO", actualizado="No se ha podiso comprobar actualizacion de script"
+#        no=Ya sale solo desde la funcion
 
-diff $descarga/$archivo_local /tmp/comprobar/$archivo_local >/dev/null 2>&1
 
-
-if [ $? = 0 ]
-then
-    #esta actualizado, solo lo comprueba
-    echo ""
-    echo -e "${verde} El script${borra_colores} $0 ${verde}esta actualizado.${borra_colores}"
-    echo ""
-    chmod -R +w /tmp/comprobar
-    rm -R /tmp/comprobar
-    actualizado="SI"
-else
-    #hay que actualizar, comprueba y actualiza
-    echo ""
-    echo -e "${amarillo} EL script${borra_colores} $0 ${amarillo}NO esta actualizado.${borra_colores}"
-    echo -e "${verde} Se procede a su actualizacion automatica.${borra_colores}"
-    sleep 3
-    mv /tmp/comprobar/$archivo_local $descarga
-    chmod -R +w /tmp/comprobar
-    rm -R /tmp/comprobar
-    echo ""
-    echo -e "${amarillo} El script se ha actualizado, es necesario cargarlo de nuevo.${borra_colores}"
-    echo -e ""
-    read -p " Pulsa una tecla para continuar." pause
-    exit
-fi
-}
-
-#funcion software necesario
-software_necesario(){
-echo ""
-echo -e " Comprobando el software necesario."
-echo ""
-software="which git diff ping figlet xdotool nano fzf" #ponemos el foftware a instalar separado por espacion dentro de las comillas ( soft1 soft2 soft3 etc )
-for paquete in $software
-do
-which $paquete 2>/dev/null 1>/dev/null 0>/dev/null #comprueba si esta el programa llamado programa
-sino=$? #recojemos el 0 o 1 del resultado de which
-contador="1" #ponemos la variable contador a 1
-    while [ $sino -gt 0 ] #entra en el bicle si variable programa es 0, no lo ha encontrado which
-    do
-        if [ $contador = "4" ] || [ $conexion = "no" ] 2>/dev/null 1>/dev/null 0>/dev/null #si el contador es 4 entre en then y sino en else
-        then #si entra en then es porque el contador es igual a 4 y no ha podido instalar o no hay conexion a internet
-            clear
-            echo ""
-            echo -e " ${amarillo}NO se ha podido instalar ${rojo}$paquete${amarillo}.${borra_colores}"
-            echo -e " ${amarillo}Intentelo usted con la orden: (${borra_colores}sudo apt install $paquete ${amarillo})${borra_colores}"
-            echo -e ""
-            echo -e " ${rojo}No se puede ejecutar el script sin el software necesario.${borra_colores}"
-            echo ""; read p
-            echo ""
-            exit
-        else #intenta instalar
-            echo " Instalando $paquete. Intento $contador/3."
-            sudo apt install $paquete -y 2>/dev/null 1>/dev/null 0>/dev/null
-            let "contador=contador+1" #incrementa la variable contador en 1
-            which $paquete 2>/dev/null 1>/dev/null 0>/dev/null #comprueba si esta el programa en tu sistema
-            sino=$? ##recojemos el 0 o 1 del resultado de which
-        fi
-    done
-echo -e " [${verde}ok${borra_colores}] $paquete."
-software="SI"
-done
-}
-
-# EMPIEZA LO GORDO
-#wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz >/dev/null 2>&1
 clear
-echo ""
+menu_info
 conexion
-echo ""
-if [ $conexion = "SI" ]
-then
-    #si hay internet
-    software_necesario
+if [ $conexion = "SI" ]; then
     actualizar_script
+    if [ $actualizado = "SI" ]; then
+        software_necesario
+        if [ $software = "SI" ]; then
+            export software="SI"
+            export conexion="SI"
+            export actualizado="SI"
+            #bash $ruta_ejecucion/ #PON LA RUTA
+        else
+            echo ""
+        fi
+    else
+        software_necesario
+        if [ $software = "SI" ]; then
+            export software="SI"
+            export conexion="NO"
+            export actualizado="No se ha podido comprobar la actualizacion del script"
+            #bash $ruta_ejecucion/ #PON LA RUTA
+        else
+            echo ""
+        fi
+    fi
 else
-    #no hay internet
     software_necesario
+    if [ $software = "SI" ]; then
+        export software="SI"
+        export conexion="NO"
+        export actualizado="No se ha podido comprobar la actualizacion del script"
+        #bash $ruta_ejecucion/ #PON LA RUTA
+    else
+        echo ""
+    fi
 fi
 
-sleep 2
 
 # Verificar si la línea existe en el archivo .bashrc
 if grep -qF 'source /home/$(whoami)/.config/alias_bashrc/alias_bashrc.config' "/home/$(whoami)/.bashrc"
